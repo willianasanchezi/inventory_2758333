ae2b00e6002878972595913df095b9b0037c8b9b
Willian Sanchez <willianasanchezi@gmail.com>
2024-08-14 00:03:56 -0500

    Se ajustan las barras de desplazamiento del menu principal

--- frontendreact/src/components/MainLayout.js
+++ frontendreact/src/components/MainLayout.js
@@ -7,10 +7,10 @@ const MainLayout = () => {
     return (
         <div className="container-fluid">
             <div className="row">
-                <div className="col-md-3">
+                <div className="col-md-3" style={{ position: 'fixed', height: '100vh', overflow: 'hidden' }}>
                     <Menu />
                 </div>
-                <div className="col-md-9">
+                <div className="col-md-9 offset-md-3" style={{ height: '100vh', overflow: 'auto' }}>
                     <Outlet />
                 </div>
             </div>
